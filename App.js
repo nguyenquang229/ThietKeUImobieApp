2a59fedd4f94221b60ef86e4c1de0455d665fdd2
quangnguyen-gtvt-229 <65327232+quangnguyen-gtvt-229@users.noreply.github.com>
2020-07-29 14:24:53 +0700

    first

---
+++ App.js
@@ -0,0 +1,39 @@
+﻿import 'react-native-gesture-handler';
+import * as React from 'react';
+import Home from './Home';
+import Dangki from './Dangki';
+import Checksuccess from './Checksuccess';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator()
+
+function App()  {
+     
+        return (
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name="Home"
+                        component={Home}
+                        options={{ title: 'Đăng nhập' }}
+                    />
+                    <Stack.Screen
+                        name="Dangki"
+                        component={Dangki}
+                        options={{title:'Đăng kí tài khoản mới' }}
+                        
+                    />
+                    <Stack.Screen
+                        name="Check"
+                        component={Checksuccess}
+                        options={{ title: 'Đăng kí tài khoản thành công' }}
+                    />
+                    
+
+                </Stack.Navigator>
+            </NavigationContainer>
+            );
+    }
+
+export default App;
---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+import App from './App'; 
+//import Dangki from './Dangki';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
